d4bc96ed64443a3fa8642803ba849dc0c82f4ebb
Frokin01 <voronsoff49@gmail.com>
2024-10-03 11:13:29 +0500

    first commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres://postgres:postgres@db:5432/tododb', {
+const sequelize = new Sequelize('postgres://default:YcwDB80QunNr@ep-proud-frost-a2y199y6.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require', {
   dialect: 'postgres',
 });
 
